2682ccea7bd48ae48efebf563677e0411fc67727
Tamanna Akter <tamannatamu457@gmail.com>
2025-08-28 15:01:13 +0600

    Create 6.multiply two floating-point numbers .c

---
+++ 6.multiply two floating-point numbers .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+   float a,b;
+   printf("Enter first float number :");
+   scanf("%f",&a);
+   printf("Enter second float number :");
+   scanf("%f",&b);
+   printf("Multiply two flating number =%.2f\n",a*b);
+    return 0;
+}
