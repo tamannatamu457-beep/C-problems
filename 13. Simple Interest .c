40b280b364ea4fddeef027ead951df71c3fc9dba
Tamanna Akter <tamannatamu457@gmail.com>
2025-09-01 22:52:52 +0600

    Create 13. Simple Interest .c

---
+++ 13. Simple Interest .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float principal, rate, time, simpleInterest;
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+    printf("Enter Rate of Interest (per year): ");
+    scanf("%f", &rate);
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+    simpleInterest = (principal * rate * time) / 100;
+    printf("Simple Interest = %.2f\n", simpleInterest);
+    return 0;
+}
