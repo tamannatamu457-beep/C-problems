d8bb5c436771b5d8c2c4952ef51ce4fa4f7f4a40
Tamanna Akter <tamannatamu457@gmail.com>
2025-11-29 19:57:34 +0600

    Create 147. Display Current Date and Time .c

---
+++ 147. Display Current Date and Time .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    printf("Date: %02d-%02d-%04d\n", tm->tm_mday, tm->tm_mon+1, tm->tm_year+1900);
+    printf("Time: %02d:%02d:%02d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+  return 0;
+}
