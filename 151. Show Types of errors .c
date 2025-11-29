32551b27835874053c70cead2a9a1fffddb517b3
Tamanna Akter <tamannatamu457@gmail.com>
2025-11-29 20:14:35 +0600

    Create 151. Show Types of errors .c

---
+++ 151. Show Types of errors .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+    int a=5,b=0; 
+if(b==0) 
+printf("Division by zero!\n"); 
+else 
+printf("%d\n",a/b);
+
+    int x=10,y=5; 
+printf("Logical Error: %d\n",x-y);
+return 0;
+}
