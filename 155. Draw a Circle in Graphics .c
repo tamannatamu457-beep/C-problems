0d23f18de0aca9ac29efda94fa98dbcee89f3ce3
Tamanna Akter <tamannatamu457@gmail.com>
2025-11-29 20:28:13 +0600

    Create 155. Draw a Circle in Graphics .c

---
+++ 155. Draw a Circle in Graphics .c
@@ -0,0 +1,13 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+    
+    circle(250, 200, 100); 
+    
+    getch();
+    closegraph();
+    return 0;
+}
