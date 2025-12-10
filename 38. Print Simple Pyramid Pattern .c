aaabebce9d13aa22cd7374cd572beae613d57e4d
Tamanna Akter <tamannatamu457@gmail.com>
2025-12-10 23:22:48 +0600

    Update 38. Print Simple Pyramid Pattern .c

--- 38. Print Simple Pyramid Pattern .c
+++ 38. Print Simple Pyramid Pattern .c
@@ -1,22 +1,10 @@
-#include <stdio.h>
-
-int main() {
-    int i, j, rows;
-
-    printf("Enter number of rows: ");
-    scanf("%d", &rows);
-
-    for (i = 1; i <= rows; i++) {
-
-        for (j = 1; j <= rows - i; j++) {
-            printf(" ");
-        }
-
-        for (j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
+#include<stdio.h>
+int main(){
+    int n, i, j;
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++) printf("*");
         printf("\n");
     }
-
     return 0;
 }
