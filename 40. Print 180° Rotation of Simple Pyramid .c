4d2c11d33c5eaab35f300571ed9c66aac72cd9e4
Tamanna Akter <tamannatamu457@gmail.com>
2025-12-10 23:15:18 +0600

    Update 40. Print 180° Rotation of Simple Pyramid .c

--- 40. Print 180° Rotation of Simple Pyramid .c
+++ 40. Print 180° Rotation of Simple Pyramid .c
@@ -1,22 +1,11 @@
-#include <stdio.h>
-
-int main() {
-    int n, i, j;
-
-    printf("Enter number of rows: ");
+#include<stdio.h>
+int main(){
+    int n, i, j, k;
     scanf("%d", &n);
-
-    for (i = n; i >= 1; i--) {
-    
-        for (j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-    
-        for (j = 1; j <= 2*i - 1; j++) {
-            printf("*");
-        }
+    for(i=1;i<=n;i++){
+        for(k=1;k<=n-i;k++) printf(" ");
+        for(j=1;j<=i;j++) printf("*");
         printf("\n");
     }
-
     return 0;
 }
